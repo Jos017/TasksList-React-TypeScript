0eb24c60cadd93c4aa53ee4037f22b52d2a89bd9
Jose Rios <jrbernabe@gmail.com>
2022-11-17 16:04:28 -0400

    Add Styles to Search bar

--- src/components/TodoSearch/index.tsx
+++ src/components/TodoSearch/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import styles from './styles.module.css';
 
 export const TodoSearch = () => {
-  return <input placeholder="Cebolla" />;
+  return (
+    <div className={styles.TodoSearch}>
+      <input placeholder="Search Task" className={styles.TodoSearch__input} />
+      <div className={styles.TodoSearch__icon}>
+        <i className={`material-icons ${styles.TodoSearch__icon__search}`}>
+          search
+        </i>
+      </div>
+    </div>
+  );
 };
