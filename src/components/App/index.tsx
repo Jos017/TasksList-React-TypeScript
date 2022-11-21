fcea655162d2305fa27775bc244655d61f183ac9
Jose Rios <jrbernabe@gmail.com>
2022-11-21 17:00:01 -0400

    Add tasks storage feature using localStorage

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { List } from '../../models/List.model';
 import { AppUI } from './AppUI';
 
-const defaultTodos: List[] = [
-  { text: 'Cortar cebolla', completed: false },
-  { text: 'Tomar el curso de intro de react', completed: true },
-  { text: 'Llorar con la llorona', completed: false },
-];
-
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos: List[];
+
+  // Verify if there are created tasks
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter((todo) => todo.completed).length;
@@ -26,25 +31,31 @@ function App() {
     });
   }
 
+  const saveTodos = (newTodos: List[]) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text: string) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const postponeTodo = (text: string) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = false;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text: string) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
